876601327e3d930d00f861b0c6c1e30199e61fc0
Duvan Candelario <duvan.programador@gmail.com>
2023-05-26 07:50:17 -0500

    Concepto de Variable, declaracion e inicializacion de variables con ejemplos

---
+++ curso_basico_javascript/variables.js
@@ -0,0 +1,26 @@
+//Variable: Es un espacio en memoria en la cual colocamos un nombre a la variable y un valor para luego acceder a ella./
+//Ejemplo:/
+var nombre = "Duvan Candelario";
+
+//HAY DOS ESTADOS DE UNA VARIABLES./
+//1. DECLARAR: RESERVAR UN ESPACIO EN MEMORIA CON UN NOMBRE/
+
+var edad
+
+//2. INICIALIZAR: ASIGNAR UN VALOR
+
+edad= 30;
+
+//OTROS EJEMPLOS. DECLARANDO EN INICIALIZANDO UNA VARIABLE
+//Ejemplo con Array (tipo objeto)
+var elementos = ["Computadora", "Celular"];
+
+//OTRA VARIABLE TIPO OBJETO.
+//ESTA ES LA FORMA DE GENERAR UN OBJETO QUE TENDRA CIERTAS CARACTERISTICAS
+var persona = {
+    nombre: "Duvan Candelario",
+    edad: 30 //no se coloca porque ya no habra nada abajo
+}
+
+
+
